9198e650797bef3d3a017c3ea65d9963b8f07e55
bgadd_brad <bradleygad@gmail.com>
2022-07-31 12:35:14 -0400

    adding some test sprites and tiled

--- level.py
+++ level.py
@@ -2,7 +2,12 @@ import pygame
 
 class Level:
     def __init__(self):
+
         self.visible_sprites = pygame.sprite.Group()
-        
+        self.obstacle_sprites = pygame.sprite.Group()
+
+    def draw_map():
+        pass
+
     def run(self):
         pass
