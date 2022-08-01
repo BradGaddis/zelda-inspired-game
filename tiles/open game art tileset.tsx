1ae9911e72760a993d2ec0d8fb5f49ea91642c64
bgadd_brad <bradleygad@gmail.com>
2022-08-01 10:12:03 -0400

    adding some tiled files

---
+++ tiles/open game art tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="open game art tileset" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../sprites/terrain_atlas.png" width="1024" height="1024"/>
+</tileset>
